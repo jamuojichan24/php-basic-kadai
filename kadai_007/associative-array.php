1303053af35d0b572a64be9ac37020f2a9b2ac77
jamuojichan24 <146048763+jamuojichan24@users.noreply.github.com>
2023-12-10 01:30:44 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎 kadai_007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $product_names = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    
+    print_r($product_names);
+    ?>
+
+  </p>
+</body>
+</html>
